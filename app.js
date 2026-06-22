91cf97d48b7825360454818370b55519ba0b4a01
meksec <meksec32@gmail.com>
2026-06-23 02:06:02 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res) => {
+    const method = req.method;
+    const url = req.url;
+    const header = req.headers;
+
+    console.log(method, url, header);
+    return res.status(200).send("OK");
+});
+
+app.listen(3000, () => console.log("Collaborator 3000 portunda hazır!"));
